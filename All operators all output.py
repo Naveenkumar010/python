832b4e0c6862e9330ebf3d1b3c343aeded288eb6
Naveenkumar010 <93249394+Naveenkumar010@users.noreply.github.com>
2022-05-14 13:19:51 +0530

    Update All operators all output.py

--- All operators all output.py
+++ All operators all output.py
@@ -11,37 +11,22 @@ def square(num1,num2):
 def exp(num1,num2):
     return num1**num2
 
-
-print("""selecte the operation to perform:
-    1.add
-    2.sub
-    3.mul
-    4.div
-    5.square
-    6.exponentential
-    """)
 Ni=int(input("No.of.inputs: "))
 for i in range(Ni):
     num1=int(input("Enter the first num : "))
     num2=int(input("enter the second num : "))
     #pass
     op=int(input("The operation to perform: "))
-    while op:
-        if op==1:
+    for i in range(op):
+        if i==1:
             print(add(num1,num2))
-            break
-        elif op==2:
+        elif i==2:
             print(sub(num1,num2))
-            break
-        elif op==3:
+        elif i==3:
             print(mul(num1,num2))
-            break
-        elif op==4:
+        elif i==4:
             print(div(num1,num2))
-            break
-        elif op==5:
+        elif i==5:
             print(square(num1,num2))
-            break
-        elif op==6:
+        elif i==6:
             print(exp(num1,num2))
-            break
